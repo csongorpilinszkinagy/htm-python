5321bbf53d309e08b82fe010a3c17c7896d83ddd
Csongor Pilinszki-Nagy <csongor.pilinszkinagy@gmail.com>
2023-05-05 11:39:41 +0000

    Scalar encoder PEP8

--- scalar_encoder.py
+++ scalar_encoder.py
@@ -1,8 +1,12 @@
 import numpy as np
 from scipy.sparse import csr_matrix, lil_matrix
 
+
 class ScalarEncoder():
-    def __init__(self, value_range: tuple[int, int], num_buckets: int, num_active_cells: int):
+    def __init__(self,
+                 value_range: tuple[int, int],
+                 num_buckets: int,
+                 num_active_cells: int):
         self.value_range = value_range
         self.num_buckets = num_buckets
         self.num_active_cells = num_active_cells
@@ -15,10 +19,10 @@ class ScalarEncoder():
     def encode(self, value: float) -> csr_matrix:
         idx = np.digitize(value, self.bins)
         sdr = lil_matrix((1, self.size), dtype=int)
-        sdr[0, idx:idx+self.num_active_cells] = 1
+        sdr[0, idx:idx + self.num_active_cells] = 1
         sdr = csr_matrix(sdr)
         return sdr
 
     def decode(self, sdr: csr_matrix) -> float:
         idx = sdr.indices[0]
-        return self.bins[max(idx-1, 0)]
+        return self.bins[max(idx - 1, 0)]
